7a882bcc101f59cf96bf30c2228179bc12e644a0
Tuukka Hastrup <Tuukka.Hastrup@iki.fi>
2019-03-04 15:45:21 +0200

    WIP: show events in TimelineJS

--- src/components/topic_page/SampoGallery.vue
+++ src/components/topic_page/SampoGallery.vue
@@ -6,6 +6,7 @@
       {{ item.relation }} {{ item.start.substring(0, 4) }}: {{ item.description }}
     </li>
   </ul>
+  <div id='timelineFIXME' :fixme='timelineData'></div>
 </div>
 </template>
 
@@ -18,6 +19,39 @@ export default {
         return {
             results: []
         };
+    },
+    computed: {
+        timelineData() {
+            var timeline = {
+                timeline: {
+                    type: 'default',
+                    date: [],
+                }
+            };
+
+            for (var i = 0; i<this.results.length; i++) {
+                var item = this.results[i];
+                timeline.timeline.date.push({
+                    startDate: item.start.substring(0, 4),
+                    headline: item.description,
+                    text: item.description,
+                });
+            }
+
+            console.log("timeline", document.getElementById('timeline'), this.results.length);
+
+            if (document.getElementById('timeline') && this.results.length > 0) {
+                createStoryJS({
+                    type:       'timeline',
+                    // width:      '800',
+                    height:     '400',
+                    source:     timeline,
+                    start_at_slide: 0 ,
+                    // start_zoom_adjust: years > 100 ? -2 : -1 ,
+                    embed_id:   'timeline'
+                });
+            }
+        }
     },
     mounted() {
         var title = this.$store.state.wikidocumentaries.title;
