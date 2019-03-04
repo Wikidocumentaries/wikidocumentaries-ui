0e87d5ce137fb1570142f3e362780903927f0aef
Mikko Rinne <mikko.rinne@iki.fi>
2019-03-04 17:32:55 +0200

    First draft ldf-sparql separated person item.

--- src/components/topic_page/SampoGallery.vue
+++ src/components/topic_page/SampoGallery.vue
@@ -28,6 +28,14 @@ export default {
             title = title.replace(/([^ ]+) ([^ ]+ )*([^ ]+)/, "$3, $1");
             part = title.split(", ")[1];
         }
+        const statements = this.$store.state.wikidocumentaries.wikidata.statements
+        let kbid;
+        for (var index in statements) {
+          if (statements[index].id == 'P2180') {
+              kbid = statements[index].values[0].value;
+          }
+        }
+        /*
         const sparql = `
 PREFIX ldfhis: <http://ldf.fi/history/sparql>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
@@ -45,7 +53,56 @@ WHERE {
     }
 }
 ORDER BY ?start
+        `; */
+
+        let sparql;
+        if (kbid) {
+        sparql = `
+    PREFIX ldfhis: <http://ldf.fi/history/sparql>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+    PREFIX kart: <http://www.kansallisbiografia.fi/kb/artikkeli/>
+    SELECT ?time ?event ?relation ?description
+    WHERE {
+      SERVICE ldfhis: {
+        {
+          ?s rdfs:seeAlso kart:` + kbid + ` .
+          ?event ?p ?s ; rdfs:label ?description .
+          ?event crm:P4_has_time-span [ crm:P82_at_some_time_within ?time ]
+        }
+        {
+           ?p rdfs:label ?relation .
+           FILTER (lang(?relation) = "fi")
+        }
+      }
+    }
+    ORDER BY ?time
         `;
+
+        } else {
+        sparql = `
+        PREFIX ldfhis: <http://ldf.fi/history/sparql>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+        SELECT DISTINCT (substr(str(?strt), 1, 4) AS ?time) ?event ?relation ?description
+        WHERE {
+          SERVICE ldfhis: {
+            {
+              ?s rdfs:label ` + JSON.stringify(title) + `@fi .
+              ?event ?p ?s ; rdfs:label ?description .
+              ?event crm:P4_has_time-span [ crm:P82a_start_of_the_beginning ?strt ]
+            }
+            {
+               ?p rdfs:label ?relation .
+               FILTER (lang(?relation) = "fi")
+            }
+          }
+        }
+        ORDER BY ?time
+        `;
+
+        }
+
         const url = wdk.sparqlQuery(sparql).replace("https://query.wikidata.org/sparql", this.$store.state.BASE_URL+"sparql");
         axios
             .get(url)
