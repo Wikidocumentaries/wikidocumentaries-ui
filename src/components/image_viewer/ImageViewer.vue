c3de187e1af77651fe2c05aa9419df15f49d5c4e
Susanna Ånäs <susanna.anas@gmail.com>
2019-04-03 18:56:05 +0300

    Applied designed styled for ImageViewer

--- src/components/image_viewer/ImageViewer.vue
+++ src/components/image_viewer/ImageViewer.vue
@@ -20,14 +20,14 @@
 							<div class="right-align">
 								<div class="toolbaricon"><i class="wikiglyph wikiglyph-cog"></i></div>
 								<div class="toolbaricon"><i class="wikiglyph wikiglyph-ellipses"></i></div>
-								<div class="toolbaricon"><i class="wikiglyph wikiglyph-cross"></i></div>
+								<div class="toolbaricon" @click="hide"><i class="wikiglyph wikiglyph-cross"></i></div>
 							</div>
 						</div>
 					</div>
 					<div class="bottomshade">
-						<div class="titlebox">
+						<div class="titlebox white">
 							<div class="titlebox-title">Kunnallispormestari ja VPK:n puheenjohtaja Elias Öhmanin (s. 28.7.1845, k. 15.3.1908) hautajaissaattue Kanavakadulla 19.3.1908</div>
-							<div class="titlebox-subtitle">Tuntematon, 1908, <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.fi">CC BY 4.0</a>. Helsingin kaupunginmuseo / <a href="https://finna.fi/Record/hkm.HKMS000005:0000083f">Finna</a></div>
+							<div class="titlebox-subtitle white">Tuntematon, 1908, <a href="https://creativecommons.org/licenses/by-sa/4.0/deed.fi">CC BY 4.0</a>. Helsingin kaupunginmuseo / <a href="https://finna.fi/Record/hkm.HKMS000005:0000083f">Finna</a></div>
 						</div>
 					</div>
 				</div>
@@ -139,7 +139,6 @@
 						</div>
 					</div>
 				</div>
-        <button @click="hide">Close</button>
 			</div>
 		</div>
     </transition>
@@ -184,18 +183,501 @@ export default {
   width: 100%;
   height: 100%;
   overflow: auto;
-  transition: opacity .3s ease;
+	transition: opacity .3s ease;
+	background: white;
+}
+
+i {
+	font-size: 24px;
 }
 
 .main-content {
-  background-color: white;
-  margin-left: 5%;
-  margin-right: 5%;
-  padding: 5%;
+  height: 100vh;
+}
+
+.headline {
+  font-family: 'Roboto Condensed', sans-serif;
+  color: #333;
+  text-transform: uppercase;
+}
+
+.main-toolbar {
+  display: -ms-flexbox;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 20px;
+}
+
+.main-toolbar-over {
+	position: absolute;
+	color: white;
+	width: 100%;
+	background: linear-gradient(360deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.25) 100%);
+}
+
+.tool {
+  background: white;
+  color: var(--main-txt-color);
+}
+
+.toolbartitle {
+  font-size: 1.3em;
+  color: white;
+}
+
+.toolbaricon {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  width: 40px;
+  transition: color 80ms ease-in, background 8ms ease-in;
+  justify-content: center;
+  cursor: pointer;
+}
+
+.toolbaricon:hover {
+    background: white;
+    color: var(--main-txt-color);
+}
+
+.stripe {
+    flex: 1 0 100%;
+    height: 18px;
+    display: flex;
+    flex-wrap: nowrap;
+}
+
+.contentarea {
+  height: calc(100vh - 18px);
+  background: black;
+  position: relative;
+}
+
+.absolute-right {
+  text-align: right;
+  height: 45px;
+  box-sizing: border-box;
+}
+
+.left-align {
+    display: flex;
+}
+
+.right-align {
+    display: flex;
+    float: right;
+    clear: both;
+    align-items: center;
+}
+
+.stroke {
+  flex: 1 1 16.67%;
+}
+
+.mw .yellow {
+    background: hsl(0, 0%, 100%);
+}
+
+.mw .orange {
+    background: hsl(0, 0%, 80%);
+}
+
+.mw .red {
+    background: hsl(0, 0%, 60%);
+}
+
+.mw .purple {
+    background: hsl(0, 0%, 40%);
+}
+
+.mw .turquoise {
+    background: hsl(0, 0%, 20%);
+}
+
+.mw .green {
+    background: hsl(0, 0%, 0%);
+}
+
+.turquoisebar {
+  height: 10px;
+  background-color: var(--main-blue);
+}
+
+.redbar {
+  height: 10px;
+  background-color: var(--main-red);
+}
+
+.greenbar {
+  height: 10px;
+  background-color: var(--main-green);
+}
+
+.topshade {
+  position: absolute;
+	width: 100%;
+	left: 0px;
+	right: 0px;
+	top: 0px;
+	background: linear-gradient(180deg, rgba(0, 0, 0, 0.4) 0%, rgba(0,0,0,0.3) 50%, rgba(0, 0, 0, 0) 100%);
+}
+.top-info {
+  display: flex;
+  margin: 15px;
+  justify-content: space-between;
+}
+
+.neg {
+  color: white;
+}
+
+.flex-col {
+  flex-direction: column;
+}
+
+.info-title {
+  font-size: 3vw;
+	font-weight: bold;
+	line-height: 1em;
+	margin-bottom: 5px;
+}
+
+.info-subtitle {
+  font-size: 1.7vw;
+	font-weight: bold;
+	line-height: 1.5em;
+}
+
+.bottomshade {
+	position: absolute;
+	width: 100%;
+	left: 0px;
+	right: 0px;
+	bottom: 0px;
+	background: linear-gradient(360deg, rgba(0, 0, 0, 0.4) 0%, rgba(0,0,0,0.3) 50%, rgba(0, 0, 0, 0) 100%);
+}
+
+.titlebox {
+	margin-left: 20px;
+	padding: 30px 0 15px;
+}
+
+.white {
+  color: white;
+}
+
+.titlebox-title {
+	font-size: 3.5vw;
+	font-weight: bold;
+	line-height: 1em;
+	margin-bottom: 5px;
+}
+
+.titlebox-subtitle {
+	font-size: 2vw;
+	font-weight: bold;
+	line-height: 1.5em;
+}
+
+.titlebox-tool {
+  margin: 30px 15px 15px;
+}
+
+.titlebox-tool-title {
+  margin-bottom: 0.2em;
+}
+
+.titlebox-tool-subtitle {
+}
+
+.titlebox-subtitle:first-letter {
+    text-transform: capitalize;
+}
+
+.titlebox-tool a {
+  color: white;
+  font-weight: bold;
+}
+
+.titlebox-tool a:hover {
+  color: var(--main-link-color);
 }
 
 .viewer-image {
-  max-width: 100%;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+}
+
+.viewer-contents {
+  position: absolute;
+  width: 100%;
+  top: 0;
+  height: calc(100vh - 18px);
+  opacity: 0;
+  transition: opacity 500ms;
+}
+
+.contentarea:hover .viewer-contents {
+  opacity: 1;  
+}
+
+.step-right {
+    background: linear-gradient(270deg, rgba(0, 0, 0, 0.3) 0%, rgba(0,0,0,0.2) 50%, rgba(0, 0, 0, 0) 100%);
+    height: 100%;
+    position: absolute;
+    right: 0;
+    top: 0;
+    opacity: 0;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.step-left {
+    background: linear-gradient(90deg, rgba(0, 0, 0, 0.3) 0%, rgba(0,0,0,0.2) 50%, rgba(0, 0, 0, 0) 100%);
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    opacity: 0;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.step-right:hover {
+  opacity: 1;
+  cursor: pointer;
+}
+
+.step-left:hover {
+  opacity: 1;
+  cursor: pointer;
+}
+
+.step-glyph {
+  font-size: 60px;
+  padding: 20px;
+}
+
+.titlebox-subtitle a {
+	color: white;
+}
+
+.titlebox-subtitle a:hover {
+	color: var(--main-link-color);
+	box-shadow: none;
+}
+
+.columns {
+	column-width: 373px;
+	column-gap: 1.5em;
+	padding: 10px 20px 15px 20px;
+}
+
+.metadata-glyph {
+  color: var(--main-txt-color);
+  font-size: 20px;
+}
+
+.grid {
+	display: grid;
+	grid-template-columns: 1fr 15fr;
+	grid-gap: .5em;
+	box-decoration-break: clone;
+	-webkit-box-decoration-break: clone;
+}
+
+.boxtitle {
+	padding: 15px 20px 0 20px;
+	font-weight: 600;
+	font-size: 1.2em;
+	text-transform: uppercase;
+}
+
+.grid-row {
+  display: flex;
+  margin-bottom: 0.5em;
+}
+
+.grid-text {
+}
+.grid-icons {
+    flex: 0 0 auto;
+    margin: 0.05em 0.8em 0 0;
+}
+.grid-item {
+	font-weight: bold;
+	display: inline;
+  color: var(--main-txt-color);
+}
+
+.metadata-map {
+  width: 100%;
+  background: var(--main-blue);
+  height: 200px;
+}
+
+.alert {
+	background-color: var(--main-yellow);
+}
+
+.alert a {
+	color: var(--main-red);
+}
+
+.alert a:hover {
+	color: var(--main-text-color);
+}
+
+.ner {
+  display: inline;
+  box-shadow: inset 0 0 0 rgba(0, 0, 0, 0), 0 3px 0 var(--main-red);
+  cursor: pointer;
+}
+
+.data-select {
+    display: inline-block;
+    background: var(--main-red);
+    color: white;
+    padding: 0 7px;
+    border-radius: 1em;
+    line-height: 1.25;
+    cursor: pointer;
+    /* transition: background 80ms ease-in, color 80ms ease-in; */
+
+}
+
+.data-select::after {
+    font-family: "WikiFont-Glyphs";
+    font-weight: 400;
+    content: " \e061";
+    font-size: 0.7em;
+}
+
+.linked {
+  color: var(--main-link-color);
+  background: white;
+}
+
+.data-select:hover, .ner:hover {
+  background: black;
+  border-radius: 0;
+  color: white;
+  box-shadow: none;
+}
+
+.unedited {
+  color: var(--main-red);
+}
+
+.unedited:hover {
+  background: black;
+  border-radius: 0;
+  color: white;
+}
+
+.unedited::after, .edited::after {
+  font-family: "WikiFont-Glyphs";
+  font-weight: 400;
+  content: " \e061";
+  font-size: 0.7em;
+}
+
+.edited {
+}
+
+.tool-main {
+  display: flex;
+  height: calc(100vh - 40px);
+}
+
+.tool-menu {
+  flex: 0 0 200px;
+  background: var(--main-txt-color);
+  padding-top: 10px;
+}
+
+.tool-menu-item {
+  display: flex;
+  height: 40px;
+  padding-left: 12px;
+  align-items: center;
+  font-weight: bold;
+  color: white;
+  cursor: pointer;
+}
+
+.tool-menu-item:hover {
+  background: white;
+  color: var(--main-txt-color)
+}
+
+.tool-menu-item-text {
+       padding: 0 15px;
+}
+
+.tool-content {
+  display: flex;
+  flex: 1 1 auto;
+  flex-direction: column;
+}
+
+.tool-workarea {
+  display: flex;
+  flex: 1 1 auto;
+}
+
+.tool-image {
+  flex: 1 1 50%;
+  background: black;
+  position: relative;
+}
+
+.toolarea {
+  flex: 1 1 50%;
+  background: var(--main-modal-color);
+}
+
+.tool-help {
+  flex: 0 0 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px 0 15px;
+}
+
+.help-text {
+  font-weight: bold;
+}
+
+.button {
+  font-weight: bold;
+  min-width: 120px;
+  background: var(--main-link-color);
+  margin-left: 10px;
+  min-height: 35px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 2px;
+  cursor: pointer;
+  box-sizing: border-box;
+}
+
+.cancel {
+  color: var(--main-blue);
+  background: white;
+}
+
+.cancel:hover {
+  border: 1px solid var(--main-blue);
 }
 
 /*
@@ -204,18 +686,21 @@ export default {
  * by Vue.js.
  */
 
-.modal-enter {
-  opacity: 0;
-}
+/* .modal-enter {
+ * opacity: 0;
+ * } 
+ */
 
-.modal-leave-active {
-  opacity: 0;
-}
+/* .modal-leave-active {
+ * opacity: 0;
+ *} 
+ */
 
-.modal-enter .modal-container,
-.modal-leave-active .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-}
+/* .modal-enter .modal-container,
+ * .modal-leave-active .modal-container {
+ *  -webkit-transform: scale(1.1);
+ *  transform: scale(1.1);
+ *} 
+ */
 
 </style>
