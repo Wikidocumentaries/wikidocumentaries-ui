ca13b364ca35a10e40e14ff96467fce5fc168139
zexigong <108034351+zexigong@users.noreply.github.com>
2023-05-30 21:31:47 -0700

    basic login

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -3,12 +3,10 @@
 </template>
 
 <script>
-import axios from "axios";
 export default {
     name: 'LoginButton',
     data(){
         return{
-            server : 'https://apiclient.wiki/',
             CLIENT_ID : "c0860cd2f1a6e322e1b4f20597c2be7a",
             CLIENT_SECRET:"0b08788ef5274e77a8b4454422cb1866d2f6a30a"
         }
@@ -19,25 +17,6 @@ export default {
             console.log(111111111111);
             let url = "https://meta.wikimedia.org/w/rest.php/oauth2/authorize?client_id=" + this.CLIENT_ID + "&response_type=code";
             window.location.href = url;
-        },
-        async lgbigfatpig(){
-            console.log(222222222222);
-            console.log(this.$route.query);
-            const params = new URLSearchParams();
-            params.append('grant_type', 'authorization_code');
-            params.append('code', code);
-            params.append('scope', 'public');
-            params.append('client_id', this.CLIENT_ID);
-            params.append('client_secret', this.CLIENT_SECRET);
-            const fetchDataRes = await axios.request({
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                url: '/w/rest.php/oauth2/access_token',
-                method: 'post',
-                baseURL: 'https://commons.wikimedia.org',
-                data: params
-            });
         },
     }
 }
--- src/components/authentication/LoginSuccess.vue
+++ src/components/authentication/LoginSuccess.vue
@@ -1,11 +1,53 @@
 <template>
-    <div>    Login Success </div>
+    <div>    Login Success 
+        <a @click="$router.go(-3)">Return to previous page</a>
+        
+    </div>
+    
     </template>
     
     <script>
-    export default {
-        name: 'LoginSuccess',
+import axios from "axios";
+export default {
+  name: "LoginSuccess",
+  data() {
+    return {
+      CLIENT_ID: "c0860cd2f1a6e322e1b4f20597c2be7a",
+      CLIENT_SECRET: "0b08788ef5274e77a8b4454422cb1866d2f6a30a",
+      profileUrl:
+        "https://meta.wikimedia.org/w/rest.php/oauth2/resource/profile"
+    };
+  },
+  methods: {
+    async getAccessToken() {
+      const code = this.$route.query.code;
+      const params = new URLSearchParams();
+      params.append("grant_type", "authorization_code");
+      params.append("code", code);
+      params.append("client_id", this.CLIENT_ID);
+      params.append("client_secret", this.CLIENT_SECRET);
+      console.log(params);
+      const fetchDataRes = await axios.request({
+        url: "/w/rest.php/oauth2/access_token",
+        method: "post",
+        baseURL: "https://meta.wikimedia.org",
+        data: params
+      });
+      console.log(fetchDataRes.data.access_token);
+      localStorage.setItem("access_token", fetchDataRes.data.access_token);
+    },
+    async getUserProfile() {
+      let response = await fetch(this.profileUrl, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("access_token")
+        }
+      });
+      response.json().then(c => localStorage.setItem("username", c.username));
     }
-    </script>
-
-    
+  },
+  mounted: async function() {
+    await this.getAccessToken();
+    await this.getUserProfile();
+  }
+};
+</script>
